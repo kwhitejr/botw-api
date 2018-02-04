e66751627bcd374a1b1bca9a34f579bc9a50304c
Kevin White <kevin.white@nike.com>
2018-02-04 09:35:53 -0800

    fixed input field style

--- client/src/App.js
+++ client/src/App.js
@@ -10,18 +10,31 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Express-React-Blueprint</h1>
         </header>
-        <Input
-          size="big"
-          className="App-intro"
-          label="http://localhost:3000"
-          placeholder="/github/:userName"
-          action={{ color: "teal", content: "Submit" }}
-        />
-        <br />
-        <div className="ui segment code-block">
-          <code>some code</code>
+        <div className="ui grid">
+          <div className="row">
+            <div className="four wide column" />
+            <div className="eight wide column">
+              <Input
+                fluid
+                className="example-input"
+                label="http://localhost:3000/"
+                placeholder="github/username"
+                action={{ color: "teal", content: "Submit" }}
+              />
+            </div>
+            <div className="four wide column" />
+          </div>
+          <div className="row">
+            <div className="four wide column" />
+            <div className="eight wide column">
+              <div className="ui segment code-block">
+                <code>some code</code>
+              </div>
+            </div>
+            <div className="four wide column" />
+          </div>
         </div>
       </div>
     );
